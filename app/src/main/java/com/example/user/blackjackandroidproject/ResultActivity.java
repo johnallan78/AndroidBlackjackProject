80ecd705664d72d89287b16b5d26f5e017691977
John Allan <johnjamesharvey@gmail.com>
2017-07-10 16:18:00 +0100

    added full deck of cards

--- app/src/main/java/com/example/user/blackjackandroidproject/ResultActivity.java
+++ app/src/main/java/com/example/user/blackjackandroidproject/ResultActivity.java
@@ -9,12 +9,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
-//
-//    TextView playerOneCardOne;
-//    TextView playerOneCardTwo;
+
     TextView playerOneHand;
-//    TextView playerTwoCardOne;
-//    TextView playerTwoCardTwo;
     TextView playerTwoHand;
     TextView finalScore;
     Button playAgainButton;
@@ -32,11 +28,8 @@ public class ResultActivity extends AppCompatActivity {
 
 
 
-//        playerOneCardOne = (TextView)findViewById(R.id.playerOneCardOne);
-//        playerOneCardTwo = (TextView)findViewById(R.id.playerOneCardTwo);
+
         playerOneHand = (TextView)findViewById(R.id.playerOneHand);
-//        playerTwoCardOne = (TextView)findViewById(R.id.playerTwoCardOne);
-//        playerTwoCardTwo = (TextView)findViewById(R.id.playerTwoCardTwo);
         playerTwoHand = (TextView)findViewById(R.id.playerTwoHand);
         playAgainButton = (Button)findViewById(R.id.playAgain);
         finalScore = (TextView)findViewById(R.id.finalScore);
@@ -57,11 +50,7 @@ public class ResultActivity extends AppCompatActivity {
         image2.setImageResource(player1card2);
 
 
-//        String answer = extras.getString("First Card");
-//        playerOneCardOne.setText(answer);
 
-//        String answerTwo = extras.getString("Second Card");
-//        playerOneCardTwo.setText(answerTwo);
 
         String hand = extras.getString("Player 1 Hand");
         playerOneHand.setText(hand);
@@ -75,11 +64,6 @@ public class ResultActivity extends AppCompatActivity {
         int player2card2 = Integer.parseInt(extras.getString("Second Card Player 2"));
         image4.setImageResource(player2card2);
 
-//        String player2 = extras.getString("First Card Player 2");
-//        playerTwoCardOne.setText(player2);
-//
-//        String player2card2 = extras.getString("Second Card Player 2");
-//        playerTwoCardTwo.setText(player2card2);
 
         String player2Hand = extras.getString("Player 2 Hand");
         playerTwoHand.setText(player2Hand);
